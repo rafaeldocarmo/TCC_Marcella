b7ffde826f09d16747f607b04ef3568dbc2facaf
Rafael Martinez do Carmo <rafael.carmo@catskillet.com>
2024-11-22 23:23:27 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import searchIcon from './imgs/searchIcon.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <img src={searchIcon} className="iconHeader" alt=""/>
+
+          <div className="App-header-center">
+            <img src={searchIcon} alt=""/>
+            <div>
+              <p>Inicio</p>
+              <p>Produtos</p>
+              <p>Contato</p>
+              <p>Quem Somos</p>
+            </div>
+          </div>
+
+          <img src={searchIcon} alt=""/>
       </header>
     </div>
   );
